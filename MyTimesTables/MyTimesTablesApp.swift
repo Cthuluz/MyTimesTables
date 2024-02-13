a5e5e4add30f76cbbd832d3f914e94baa7600b4d
Isabel Nelson <inels19@wgu.edu>
2024-02-13 09:35:38 -0700

    Initial Commit

---
+++ MyTimesTables/MyTimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimesTablesApp.swift
+//  MyTimesTables
+//
+//  Created by Isabel Nelson on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
