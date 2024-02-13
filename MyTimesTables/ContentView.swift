22257ae82ea1553122f6eb9bb398177cc961113c
Isabel Nelson <inels19@wgu.edu>
2024-02-13 11:06:57 -0700

    Add background color

--- MyTimesTables/ContentView.swift
+++ MyTimesTables/ContentView.swift
@@ -13,38 +13,42 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    Stepper("Select maximum times table:", value: $upTo, in: 2...12)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 20)
-                    Text("\(upTo)")
-                        .padding(.trailing, 50)
-                }
-                .padding([.top, .bottom], 50)
-                
+            ZStack {
+                Color(red: 240/255, green: 243/255, blue: 255/255)
                 VStack {
-                    Text("Select number of questions:")
-                        .font(.body)
-                        .frame(alignment: .leading)
-                    
-                    Picker("Number of questions", selection: $numberOfQuestions) {
-                        ForEach([5, 10, 20], id: \.self) { number in
-                            Text("\(number)")
-                        }
+                    HStack {
+                        Stepper("Select maximum times table:", value: $upTo, in: 2...12)
+                            .padding(.leading, 50)
+                            .padding(.trailing, 20)
+                        Text("\(upTo)")
+                            .padding(.trailing, 50)
                     }
-                    .pickerStyle(.segmented)
-                }
-                .padding([.leading, .trailing], 50)
-                
-                Button("Start") {
+                    .padding([.top, .bottom], 50)
                     
+                    VStack {
+                        Text("Select number of questions:")
+                            .font(.body)
+                            .frame(alignment: .leading)
+                        
+                        Picker("Number of questions", selection: $numberOfQuestions) {
+                            ForEach([5, 10, 20], id: \.self) { number in
+                                Text("\(number)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    .padding([.leading, .trailing], 50)
+                    
+                    Button("Start") {
+                        
+                    }
+                    .padding([.top, .bottom], 50)
+                    .frame(alignment: .leading)
+                    .buttonStyle(.bordered)
                 }
-                .padding([.top, .bottom], 50)
-                .frame(alignment: .leading)
-                .buttonStyle(.bordered)
+                .navigationTitle("MyTimesTables")
             }
-            .navigationTitle("MyTimesTables")
+            .ignoresSafeArea()
         }
     }
 }
